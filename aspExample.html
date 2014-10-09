a006d8ac215677ff9ee645520207806c87504de6
Anita Mehrotra <anitamehrotra@AnitaMehrotra-MBP.local>
2014-10-09 10:17:10 -0400

    some changes

---
+++ aspExample.html
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<body>
+<%
+response.write("My first ASP script!")
+%>
+</body>
+</html>
--- k12seclRest.js
+++ k12seclRest.js
@@ -3,4 +3,32 @@
  * @date: October 3, 2014
  */
 
- 
+var margin = {
+ 	top: 50,
+ 	right: 50,
+ 	bottom: 50,
+ 	left: 50
+};
+
+var width = 960 - margin.left - margin.right,
+	height = 1160 - margin.bottom - margin.top,
+	centered;
+
+var svg = d3.select("body").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+
+
+function updateData() {
+	console.log("updateData()!")
+}
+// d3.json("data/us-named.json", function(error, usa) {
+
+// 	if (error) return console.error(error);
+
+
+// });
+
+
+
+
