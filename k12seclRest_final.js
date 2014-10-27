c1b7d056a72d14ec48cf4f9ea4a643283305706d
Anita Mehrotra <anitamehrotra@AnitaMehrotra-MBP.local>
2014-10-27 14:34:06 -0400

    recreated after deleting :(

--- k12seclRest_final.js
+++ k12seclRest_final.js
@@ -35,8 +35,8 @@ d3.json("data/us-named.json", function(error, us) {
 		.selectAll("path")
 			.data(usMap)
 		.enter().append("path")
-			.attr("d", path);
-			// .on("click", clicked);
+			.attr("d", path)
+			.on("click", clicked);
 
 	svg.append("g").append("path")
 		.datum(topojson.mesh(us, us.objects.states, function(a,b) { return a !== b; }))	
@@ -44,3 +44,97 @@ d3.json("data/us-named.json", function(error, us) {
 		.attr("d", path);
 
 });
+
+function clicked(d) {
+	// change state color
+	d3.select(".active").classed("active", false)
+	d3.select(this).classed("active", true)
+
+	// remove old table
+	
+	// get state data
+	var state = d.properties.code;
+	var pathname = "data/" + state + ".csv";
+	
+	d3.csv(pathname, function(error, data) {
+		var stateData = getSeclRest(data); // get punishment data
+		var myTable = makeTable(stateData); // make table
+		
+	});
+}
+
+function getSeclRest(data) {
+	var alldata = [];
+	var j = 0;
+	console.log(j);
+	data.forEach(function(d) {
+		var TE = parseInt(d.total_enrollment);
+		var mech_dis = ( parseInt(d.idea_mech_restraints) + parseInt(d.section_504_mech_restraints) )/TE;
+		var mech_no_dis = parseInt(d.no_dis_mech_restraints)/TE;
+		var phys_dis = ( parseInt(d.idea_phys_restraints) + parseInt(d.section_504_phys_restraints) )/TE;
+		var phys_no_dis = parseInt(d.no_dis_phys_restraints)/TE;
+		var secl_dis = ( parseInt(d.idea_seclusion) + parseInt(d.section_504_seclusion) )/TE;
+		var secl_no_dis = parseInt(d.no_dis_seclusion)/TE;
+
+		if ( (mech_dis != 0 && mech_dis <= 1) || (mech_no_dis != 0 && mech_no_dis <= 1) &&
+			 (phys_dis != 0 && phys_dis <= 1) || (phys_no_dis != 0 && phys_no_dis <= 1) &&
+			 (secl_dis != 0 && secl_dis <= 1) || (secl_no_dis != 0 && secl_no_dis <= 1) ) {
+
+			if (mech_dis != 0) { mech_dis = parseFloat(mech_dis).toFixed(3) };
+			if (mech_no_dis != 0) { mech_no_dis = parseFloat(mech_no_dis).toFixed(3) };
+			if (phys_dis != 0) { phys_dis = parseFloat(phys_dis).toFixed(3) };
+			if (phys_no_dis != 0) { phys_no_dis = parseFloat(phys_no_dis).toFixed(3) };
+			if (secl_dis != 0) { secl_dis = parseFloat(secl_dis).toFixed(3) };
+			if (secl_no_dis != 0) { secl_no_dis = parseFloat(secl_no_dis).toFixed(3) };
+
+			alldata[j] = [d.School_name, TE, mech_dis, mech_no_dis, phys_dis, phys_no_dis, secl_dis, secl_no_dis];
+			console.log(j);
+			j = j+1; 
+
+		}
+	});
+	return alldata;
+}
+
+function makeTable(data) {
+	var column_names = ["School", "Total Enrollment", "Mechanical, Disabled", "Physical, Disabled", "Seclusions, Disabled",
+						"Mechanical, Not Disabled", "Physical, Not Disabled", "Seclusions, Not Disabled"];
+	
+	// make table framework
+	var table = d3.select("body").append("table")
+			.style("margin-left", "50px")
+		thead = table.append("thead"),
+		tbody = table.append("tbody");
+
+	// append header row
+	thead.append("tr")
+		.select("th")
+		.data(column_names)
+		.enter()
+		.append("th")
+			.text(function(d) { return d; });
+				.style("font-family", "Impact");
+
+	// create a row for each object in the data
+	var rows = tbody.selectAll("tr")
+		.data(data)
+		.enter()
+		.append("tr");
+
+
+	// create a cell in each row for each column
+	var cells = rows.selectAll("td")
+		.data(function(row) {
+			return column_names.map(function(column, i) {
+				console.log("row[i]: " + row[i]);
+				return { column:column, value:row[i] };
+			});
+		})
+		.enter()
+		.append("td")
+		.attr("style", "font-family: sans-serif")
+			.html(function(d) { return d.value; });
+
+	return table;
+
+}
